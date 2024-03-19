e5da09421715a42172385e58fa10f58a400c4f23
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-03-19 12:36:54 +0530

    Time: 8 ms (73.19%) | Memory: 20.4 MB (34.46%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.cpp
+++ 777-toeplitz-matrix/toeplitz-matrix.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-         int m = matrix.size();
+        int m = matrix.size();
         int n = matrix[0].size();
-        
-        // Iterate through each element of the matrix except for the first row and first column
+
+        // Iterate matrix except the first row and first column
+
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
-                // Check if the current element matches the one directly above and to the left of it
-                if (matrix[i][j] != matrix[i-1][j-1]) {
-                    return false; // If not, return false
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
                 }
             }
         }
         return true;
-        
     }
 };
