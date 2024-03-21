35525653ab1ce93c458e69298583f39bd610ed43
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-03-21 13:17:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (34.51%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,17 +1,35 @@
-// time O(N) , aux space O(N)
+// // time O(N) , aux space O(N)
+// // reverse with a vector
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         vector<int> data;
+
+//         for (ListNode* curr = head; curr != NULL; curr = curr->next) {
+//             data.push_back(curr->val);
+//         }
+
+//         for (ListNode* curr = head; curr != NULL; curr = curr->next) {
+//             curr->val = data.back();
+//             data.pop_back();
+//         }
+//         return head;
+//     }
+// };
+
+// three pointer approach
+// iterative approach
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        vector<int> data;
+        ListNode *prev = NULL, *curr = head;
 
-        for (ListNode* curr = head; curr != NULL; curr = curr->next) {
-            data.push_back(curr->val);
+        while (curr != NULL) {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
         }
-
-        for (ListNode* curr = head; curr != NULL; curr = curr->next) {
-            curr->val = data.back();
-            data.pop_back();
-        }
-        return head;
+        return prev;
     }
 };
