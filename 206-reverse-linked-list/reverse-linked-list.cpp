6d78475d1a3885b2807dd0b28b246974286550bc
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-03-21 13:03:04 +0530

    Time: 3 ms (79.91%) | Memory: 11.7 MB (15.82%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,28 +1,17 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
- 
+// time O(N) , aux space O(N)
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode *prev = nullptr;
-        ListNode *current = head;
-        ListNode *next = nullptr;
+        vector<int> data;
 
-        while (current != nullptr) {
-            next = current->next; // Store the next node
-            current->next = prev; // Reverse the pointer direction
-            prev = current;       // Move prev pointer forward
-            current = next;       // Move current pointer forward
+        for (ListNode* curr = head; curr != NULL; curr = curr->next) {
+            data.push_back(curr->val);
         }
 
-        return prev; // prev now points to the head of the reversed list
+        for (ListNode* curr = head; curr != NULL; curr = curr->next) {
+            curr->val = data.back();
+            data.pop_back();
+        }
+        return head;
     }
 };
