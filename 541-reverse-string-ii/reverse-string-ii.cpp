b6b7658af54a7ee1809a0f4e88f63bc46c6743ed
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-03-24 11:08:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (36.87%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.cpp
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -3,10 +3,14 @@ public:
     string reverseStr(string s, int k) {
         int n = s.size();
 
+        // Traverse the string with step 2*k
         for (int i = 0; i < n; i += 2 * k) {
+            // if fewer than k char left, reverse all remaining char
             if ((n - i) < k) {
                 reverse(s.begin() + i, s.end());
-            } else {
+            } 
+            // if k or more char left, reverse the first k char
+            else {
                 reverse(s.begin() + i, s.begin() + i + k);
             }
         }
