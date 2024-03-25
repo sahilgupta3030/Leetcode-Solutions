167e8d941323582803eb7135015a8a114fab2105
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-03-25 22:18:52 +0530

    Time: 1098 ms (21.48%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,18 +1,18 @@
-SELECT w1.id AS Id
-FROM Weather w1
-INNER JOIN Weather w2
-ON w1.recordDate - INTERVAL 1 DAY = w2.recordDate
-WHERE w1.temperature > w2.temperature;
-
-
-
-
-
-
-
-
 -- SELECT w1.id AS Id
--- FROM weather w1
--- INNER JOIN weather w2 
--- ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+-- FROM Weather w1
+-- INNER JOIN Weather w2
+-- ON w1.recordDate - INTERVAL 1 DAY = w2.recordDate
 -- WHERE w1.temperature > w2.temperature;
+
+
+
+
+
+
+
+
+SELECT w1.id AS Id
+FROM weather w1
+INNER JOIN weather w2 
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
