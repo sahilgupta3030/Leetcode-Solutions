0b8e352926939b7d3b83d6d13f578779a8817951
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-03-26 21:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (39.08%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int n = tickets.size(); // Size of tickets array
-        queue<int> q; // Queue for people buying tickets
+        queue<int>q;
+        for(int i=0;i<tickets.size();i++)
+        q.push(i);
 
-        for (int i = 0; i < n; ++i) {
-            q.push(i); // Initialize queue
+        int timer =0;
+
+        while(tickets[k]!=0)
+        {
+            tickets[q.front()]--;
+            if(tickets[q.front()])
+            q.push(q.front());
+            q.pop();
+            timer++;
         }
 
-        int times = 0; // Total time taken
-
-        while (true) {
-            bool allBought = true; // Check if all tickets bought
-            for (int i = 0; i < n; ++i) {
-                if (tickets[i] > 0) { // Check if tickets left
-                    allBought = false;
-                    break;
-                }
-            }
-            if (allBought) // Break if all tickets bought
-                break;
-
-            int curr = q.front(); // Current person
-            q.pop(); // Remove from queue
-            int ticketsToBuy = min(tickets[curr], 1); // Buy one ticket at a time
-            times += ticketsToBuy; // Update time
-            tickets[curr] -= ticketsToBuy; // Update tickets count
-            if (curr == k && tickets[curr] == 0) // If person at k bought all tickets
-                break;
-            if (tickets[curr] > 0) // Add back to queue if tickets left
-                q.push(curr);
-        }
-        return times; // Return total time taken
+        return timer;
     }
 };
