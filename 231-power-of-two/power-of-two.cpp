80e22eb7a14779a1ca41387bc2a4041d120ffbfc
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-03-26 14:38:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (77.04%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,23 +1,31 @@
+// BIT MAGIC
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
-        if (n == 1)
-            return true; // Base case: n equals 1
-
-        
-        if (n < 1 || n % 2 != 0)
-            return false; // n less than 1 or odd
-
-        // Iterative: divide n by 2 until it's not divisible
-        while (n > 1) {
-            if (n % 2 != 0)
-                return false;
-            n /= 2;
-        }
-        return true;
+        // Check if n is positive and has only one bit set
+        return n > 0 && (n & (n - 1)) == 0;
     }
 };
+
+// // ITERATIVE
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         if (n == 1)
+//             return true; // Base case: n equals 1
+
+//         if (n < 1 || n % 2 != 0)
+//             return false; // n less than 1 or odd
+
+//         // Iterative: divide n by 2 until it's not divisible
+//         while (n > 1) {
+//             if (n % 2 != 0)
+//                 return false;
+//             n /= 2;
+//         }
+//         return true;
+//     }
+// };
 
 // // RECURSIVE
 // class Solution {
