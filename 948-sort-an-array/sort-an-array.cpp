47a94a8f613cf8ddf308b4cbc000dad16f7850a4
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-03-29 11:33:57 +0530

    Time: 242 ms (52.05%) | Memory: 142.6 MB (43.70%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,54 +1,41 @@
 class Solution {
-public:
-    void merge(vector<int>& nums, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        
-        vector<int> L(n1), R(n2);
-        
-        for (int i = 0; i < n1; i++)
-            L[i] = nums[left + i];
-        for (int j = 0; j < n2; j++)
-            R[j] = nums[mid + 1 + j];
-        
-        int i = 0, j = 0, k = left;
-        
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                nums[k] = L[i];
-                i++;
-            } else {
-                nums[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-        
-        while (i < n1) {
-            nums[k] = L[i];
-            i++;
-            k++;
-        }
-        
-        while (j < n2) {
-            nums[k] = R[j];
-            j++;
-            k++;
-        }
-    }
+ public:
+  vector<int> sortArray(vector<int>& nums) {
+    mergeSort(nums, 0, nums.size() - 1);
+    return nums;
+  }
 
-    void mergeSort(vector<int>& nums, int left, int right) {
-        if (left >= right)
-            return;
-        
-        int mid = left + (right - left) / 2;
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
-        merge(nums, left, mid, right);
-    }
+ private:
+  void mergeSort(vector<int>& A, int l, int r) {
+    if (l >= r)
+      return;
 
-    vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
-        return nums;
-    }
+    const int m = (l + r) / 2;
+    mergeSort(A, l, m);
+    mergeSort(A, m + 1, r);
+    merge(A, l, m, r);
+  }
+
+  void merge(vector<int>& A, int l, int m, int r) {
+    vector<int> sorted(r - l + 1);
+    int k = 0;      // sorted's index
+    int i = l;      // left's index
+    int j = m + 1;  // right's index
+
+    while (i <= m && j <= r)
+      if (A[i] < A[j])
+        sorted[k++] = A[i++];
+      else
+        sorted[k++] = A[j++];
+
+    // Put the possible remaining left part into the sorted array.
+    while (i <= m)
+      sorted[k++] = A[i++];
+
+    // Put the possible remaining right part into the sorted array.
+    while (j <= r)
+      sorted[k++] = A[j++];
+
+    copy(sorted.begin(), sorted.end(), A.begin() + l);
+  }
 };
