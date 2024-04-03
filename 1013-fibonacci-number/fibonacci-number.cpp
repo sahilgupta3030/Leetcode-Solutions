914f7e51c9a35e71d38cb66007d3e2e9a4dfde80
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-03 13:12:26 +0530

    Time: 2 ms (79.59%) | Memory: 7.2 MB (12.53%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,11 +1,27 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n<2)
+//         return n;
+//         return fib(n-1)+fib(n-2);
+//     }
+// };
+
 class Solution {
 public:
     int fib(int n) {
+        if (n <= 1)
+            return n;
 
-        if(n<2)
-        return n;
+        int t1 = 0, t2 = 1, nextTerm = 0;
 
-        return fib(n-1)+fib(n-2);
-        
+        while (n > 1) {
+            nextTerm = t1 + t2;
+            t1 = t2;
+            t2 = nextTerm;
+            n--;
+        }
+        return t2;
     }
 };
+
