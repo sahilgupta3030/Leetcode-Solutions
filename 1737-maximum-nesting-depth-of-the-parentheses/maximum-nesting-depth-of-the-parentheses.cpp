acd963cb6c07268f551bb7f0ecce5545f097452a
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-04 19:37:27 +0530

    Time: 3 ms (40.00%) | Memory: 7.4 MB (78.96%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,46 +1,46 @@
-// Approach-1 (using stack)
-// TC: O(n) SC: O(n)
-class Solution {
-public:
-    int maxDepth(string s) {
-        int result = 0;
-
-        stack<char> st;
-        
-        for (char& ch : s) {
-            
-            if (ch == '(') {
-                st.push(ch);
-            } else if (ch == ')') {
-                st.pop();
-            }
-            
-            result = max(result, (int)st.size());
-        }
-        
-        return result;
-    }
-};
-
-
-// // Approach-2 (using constant space)
-// // TC: O(n) SC: O(1)
+// // Approach-1 (using stack)
+// // TC: O(n) SC: O(n)
 // class Solution {
 // public:
 //     int maxDepth(string s) {
 //         int result = 0;
-//         int openBrackets = 0;
 
-//         for (char &ch : s) {
+//         stack<char> st;
+        
+//         for (char& ch : s) {
+            
 //             if (ch == '(') {
-//                 openBrackets++;
+//                 st.push(ch);
 //             } else if (ch == ')') {
-//                 openBrackets--;
+//                 st.pop();
 //             }
             
-//             result = max(result, openBrackets);
+//             result = max(result, (int)st.size());
 //         }
         
 //         return result;
 //     }
 // };
+
+
+// Approach-2 (using constant space)
+// TC: O(n) SC: O(1)
+class Solution {
+public:
+    int maxDepth(string s) {
+        int result = 0;
+        int openBrackets = 0;
+
+        for (char &ch : s) {
+            if (ch == '(') {
+                openBrackets++;
+            } else if (ch == ')') {
+                openBrackets--;
+            }
+            
+            result = max(result, openBrackets);
+        }
+        
+        return result;
+    }
+};
