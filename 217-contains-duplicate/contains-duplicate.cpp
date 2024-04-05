be71f951c2c4a5bbcbf26e905364fc7315ee6727
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-05 15:00:44 +0530

    Time: 85 ms (80.67%) | Memory: 60.7 MB (93.01%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // Sets in C++ store only unique elements
-        set<int> s(nums.begin(),nums.end());
-        int x=nums.size();
-        int y=s.size();
-
-        if(x==y){
-            return false;
+        // Sorting the array
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] == nums[i + 1])
+                return true;
         }
-        else{
-            return true;
-        }
+        return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         // Sets in C++ store only unique elements
+//         set<int> s(nums.begin(),nums.end());
+//         int x=nums.size();
+//         int y=s.size();
+
+//         if(x==y){
+//             return false;
+//         }
+//         else{
+//             return true;
+//         }
+//     }
+// };
