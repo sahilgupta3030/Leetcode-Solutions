e1e3ef5b791b26cc1bd61d6c38fe8d785876a278
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-06 22:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (60.64%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,53 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st; 
-        for(auto i:s) 
-        {
-            if(i=='(' or i=='{' or i=='[') {
-                st.push(i);  
+        stack<char> st;
+        
+        for (auto i : s) {
+            if (i == '(' or i == '{' or i == '[') {
+                st.push(i);
             }
-           
-            else  
-            {
-                if(st.empty() or (st.top()=='(' and i!=')') or (st.top()=='{' and i!='}') or (st.top()=='[' and i!=']')) {
-                   return false;
+
+            else {
+                if (st.empty() or (st.top() == '(' and i != ')') or
+                    (st.top() == '{' and i != '}') or
+                    (st.top() == '[' and i != ']')) {
+                    return false;
+                } else {
+                    st.pop();
                 }
-                else{
-                   st.pop();
-                }
-                  
             }
         }
-        return st.empty();  
+        return st.empty();
     }
 };
-
-
-
-// class Solution {
-// public:
-//     bool isValid(string s) {
-
-//         if (s.empty())
-//             return false;
-
-//         stack<char> st;
-//         for (auto i : s) {
-//             if (i == '(' or i == '{' or i == '[') {
-//                 st.push(i);
-//             }
-
-//             else {
-//                 if (!st.empty() && ((st.top() == '(' and i == ')') or
-//                                     (st.top() == '{' and i == '}') or
-//                                     (st.top() == '[' and i == ']'))) {
-//                     st.pop();
-//                 } else {
-//                     return false;
-//                 }
-//             }
-//         }
-//         return st.empty();
-//     }
-// };
