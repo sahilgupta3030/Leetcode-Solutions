eaf8aa9538b13898e896c7aef325f86ac3cc5f00
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-06 21:49:46 +0530

    Time: 17 ms (84.34%) | Memory: 11.7 MB (86.43%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -4,13 +4,21 @@ public:
         stack<int> st;
 
         for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '(') st.push(i);
-            else if (s[i] == ')' && !st.empty()) st.pop();
-            else if (s[i] == ')') s[i] = '*';
+            if (s[i] == '(')
+                st.push(i);
+            else if (s[i] == ')' && !st.empty())
+                st.pop();
+            else if (s[i] == ')')
+                s[i] = '@'; // problematic bracket index stored
         }
-        
-        while (!st.empty()) { s[st.top()] = '*'; st.pop(); }
-        s.erase(remove(s.begin(), s.end(), '*'), s.end());
+
+        while (!st.empty()) {
+            s[st.top()] = '@'; // deleting
+            st.pop();
+        }
+
+        // remove @ from string
+        s.erase(remove(s.begin(), s.end(), '@'), s.end());
         return s;
     }
 };
