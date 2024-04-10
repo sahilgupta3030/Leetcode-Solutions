27a3cb624669f32c32a4f1d629424589b8b9345b
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-10 23:10:43 +0530

    Time: 6 ms (30.99%) | Memory: 10.8 MB (91.52%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -1,16 +1,25 @@
 class Solution {
- public:
-  vector<int> deckRevealedIncreasing(vector<int>& deck) {
-    ranges::sort(deck, greater<int>());
-
-    deque<int> dq{deck[0]};
-
-    for (int i = 1; i < deck.size(); ++i) {
-      dq.push_front(dq.back());
-      dq.pop_back();
-      dq.push_front(deck[i]);
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& v) {
+        sort(v.begin(),v.end());
+        queue<int>q;
+        int n=v.size();
+        for(int i=0;i<n;i++) {
+            q.push(i);
+        }
+        vector<int>ans(n,0);
+        int i=0;
+        while(!q.empty()) {
+            int k=q.front();
+            ans[k]=v[i];
+            q.pop();
+            if(!q.empty()) {
+                int x=q.front();
+                q.push(x);
+                q.pop();
+            }
+            i++;
+        }
+        return ans;
     }
-
-    return {dq.begin(), dq.end()};
-  }
 };
