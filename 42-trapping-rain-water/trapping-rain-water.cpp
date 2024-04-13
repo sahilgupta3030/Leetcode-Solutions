8e988d828815f949773a55875eec3f041d926abf
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-13 23:38:22 +0530

    Time: 7 ms (88.12%) | Memory: 23.2 MB (25.33%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int leftMax = 0, rightMax = 0, ans = 0;
-        int left = 0, right = height.size() - 1;
-        
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] < leftMax) { // Can contain water
-                    ans += leftMax - height[left];
-                } else {
-                    leftMax = height[left];
-                }
-                left++;
-            } else {
-                if (height[right] < rightMax) { // Can contain water
-                    ans += rightMax - height[right];
-                } else {
-                    rightMax = height[right];
-                }
-                right--;
-            }
+    vector<int> getLeftMax(vector<int>& height, int n) {
+        vector<int> leftMax(n);
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = max(leftMax[i - 1], height[i]);
         }
-        return ans;
+        return leftMax;
+    }
+
+    vector<int> getRightMax(vector<int>& height, int n) {
+        vector<int> rightMax(n);
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = max(rightMax[i + 1], height[i]);
+        }
+        return rightMax;
+    }
+
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n == 1 || n == 0)
+            return 0;
+            
+        vector<int> leftMax = getLeftMax(height, n);
+        vector<int> rightMax = getRightMax(height, n);
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return sum;
     }
 };
+
