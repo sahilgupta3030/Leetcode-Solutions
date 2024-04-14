67e97acc5d550ed8658788489f6ba1b74ec33bd5
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-14 14:45:45 +0530

    Time: 8 ms (42.00%) | Memory: 17.6 MB (10.43%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
+        // If the tree is empty, its depth is 0
         if (root == nullptr)
             return 0;
 
+        // Queue to store nodes for BFS traversal
         queue<TreeNode*> nodes;
+        // Start with the root node
         nodes.push(root);
-        int levels = 0;
+        // Initialize depth to 0
+        int depth = 0;
 
+        // Perform BFS traversal
         while (!nodes.empty()) {
-            levels++;
+            // Increment depth for each level
+            depth++;
+            // Get the number of nodes at the current level
             int size = nodes.size();
+            // Process all nodes at the current level
             for (int i = 0; i < size; i++) {
+                // Get the front node and remove it from the queue
                 TreeNode* poppedNode = nodes.front();
                 nodes.pop();
+                // Add child nodes to the queue for the next level
                 if (poppedNode->left != nullptr)
                     nodes.push(poppedNode->left);
                 if (poppedNode->right != nullptr)
@@ -21,6 +31,7 @@ public:
             }
         }
 
-        return levels;
+        // Return the maximum depth of the tree
+        return depth;
     }
 };
