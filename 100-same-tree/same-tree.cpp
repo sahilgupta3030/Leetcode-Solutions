e18d58f564196cbff0aa62434027753e3f09d0b1
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-14 14:18:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (31.71%) - LeetSync

---
+++ 100-same-tree/same-tree.cpp
@@ -0,0 +1,18 @@
+//Approach-1 (Using DFS)
+//T.C : O(n)
+//S.C : O(1) Auxiliary space, O(depth of tree) Recursion stack space
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q)
+            return true;
+        
+        if(p == NULL || q == NULL)
+            return false;
+        
+        if(p->val != q->val)
+            return false;
+        
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
