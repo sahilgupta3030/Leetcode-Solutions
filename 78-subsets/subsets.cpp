d69c96b7119d6593736f5b609d8e7037641a2a16
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-19 18:43:38 +0530

    Time: 4 ms (39.97%) | Memory: 8.2 MB (94.90%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
+    // Generates all subsets of a given set of numbers
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> resultList;
         vector<int> tempSet;
 
+        // Backtrack to generate subsets
         backtrack(resultList, tempSet, nums, 0);
 
         return resultList;
     }
 
 private:
+    // Recursive function to generate subsets
     void backtrack(vector<vector<int>>& resultSets, vector<int>& tempSet,
                    vector<int>& nums, int start) {
+        // Add current subset to result
         resultSets.push_back(tempSet);
 
+        // Generate subsets starting from 'start' index
         for (int i = start; i < nums.size(); i++) {
             tempSet.push_back(nums[i]);
             backtrack(resultSets, tempSet, nums, i + 1);
