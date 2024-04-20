df630743af9f47d258161d8800d3cce9927d20a8
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-20 12:42:15 +0530

    Time: 86 ms (95.64%) | Memory: 59.3 MB (84.36%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,30 +1,40 @@
 class Solution {
- public:
-  vector<vector<int>> findFarmland(vector<vector<int>>& land) {
-    vector<vector<int>> ans;
+public:
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        vector<vector<int>> ans;
 
-    for (int i = 0; i < land.size(); ++i)
-      for (int j = 0; j < land[0].size(); ++j)
-        if (land[i][j] == 1) {
-          int x = i;
-          int y = j;
-          dfs(land, i, j, x, y);
-          ans.push_back({i, j, x, y});
-        }
+        for (int i = 0; i < land.size(); ++i)
+            for (int j = 0; j < land[0].size(); ++j)
+                if (land[i][j] == 1) {
+                    int x = i;
+                    int y = j;
 
-    return ans;
-  }
+                    // Explore the farmland using DFS
+                    dfs(land, i, j, x, y);
 
- private:
-  void dfs(vector<vector<int>>& land, int i, int j, int& x, int& y) {
-    if (i < 0 || i == land.size() || j < 0 || j == land[0].size())
-      return;
-    if (land[i][j] != 1)
-      return;
-    land[i][j] = 2;  // Mark as visited.
-    x = max(x, i);
-    y = max(y, j);
-    dfs(land, i + 1, j, x, y);
-    dfs(land, i, j + 1, x, y);
-  }
+                    // Add the coordinates of the farmland to the result
+                    ans.push_back({i, j, x, y});
+                }
+
+        return ans;
+    }
+
+private:
+    void dfs(vector<vector<int>>& land, int i, int j, int& x, int& y) {
+        // Base cases
+        if (i < 0 || i == land.size() || j < 0 || j == land[0].size())
+            return; // Out of bounds
+        if (land[i][j] != 1)
+            return; // Not part of farmland
+
+        land[i][j] = 2; // Mark current cell as visited
+
+        // Update the boundaries of the farmland
+        x = max(x, i);
+        y = max(y, j);
+
+        // Explore adjacent cells
+        dfs(land, i + 1, j, x, y); // Down
+        dfs(land, i, j + 1, x, y); // Right
+    }
 };
