678a86df3538997bdf5b984f453f7b17fac36043
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-22 16:46:52 +0530

    Time: 132 ms (70.02%) | Memory: 38.8 MB (56.90%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -2,33 +2,37 @@ class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
         unordered_set<string> dead(deadends.begin(), deadends.end());
-        if (dead.count("0000")) return -1;
-        
-        queue<string> q;
         unordered_set<string> visited;
         
+        queue<string> q;
         q.push("0000");
         visited.insert("0000");
-        
         int level = 0;
-        while (!q.empty()) {
-            int sz = q.size();
-            for (int i = 0; i < sz; ++i) {
-                string cur = q.front(); q.pop();
-                if (cur == target) return level;
-                if (dead.count(cur)) continue;
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                string current = q.front();
+                q.pop();
+                if(current == target) return level;
+                if(dead.count(current)) continue;
                 for (int j = 0; j < 4; ++j) {
-                    for (int k = -1; k <= 1; k += 2) {
-                        string next = cur;
-                        next[j] = (next[j] - '0' + k + 10) % 10 + '0';
-                        if (!visited.count(next)) {
-                            q.push(next);
-                            visited.insert(next);
-                        }
+                    string up = current;
+                    string down = current;
+                    
+                    up[j] = (up[j] - '0' + 1) % 10 + '0';
+                    down[j] = (down[j] - '0' + 9) % 10 + '0';
+                    
+                    if (!visited.count(up)) {
+                        q.push(up);
+                        visited.insert(up);
                     }
-                }
+                    if (!visited.count(down)) {
+                        q.push(down);
+                        visited.insert(down);
+                    }
+                }   
             }
-            ++level;
+            level++;
         }
         return -1;
     }
