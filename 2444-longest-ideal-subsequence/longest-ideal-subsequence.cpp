7026f400ea9593722d8ad83c9faedf26160c3a53
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-25 10:41:35 +0530

    Time: 33 ms (94.54%) | Memory: 11.5 MB (84.45%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -1,32 +1,30 @@
-// Define a class Solution
 class Solution {
 public:
-    // Define a function longestIdealString with parameters s and k
     int longestIdealString(string s, int k) {
 
         // Initialize a vector dp of size 26 with 0s
-        vector<int> dp(26,0);
-        // Initialize ans as 1
-        int ans=1;
+        vector<int> dp(26, 0);
+        int ans = 1;
 
         // Iterate through the characters of string s
-        for(int i=0;i<s.size();i++){
+        for (int i = 0; i < s.size(); i++) {
 
             // Calculate the range of characters to consider
-            int x=max(0,s[i]-'a'-k);
-            int y=min(25,s[i]-'a'+k);
-            // Initialize temp as 1
-            int temp=1;
+            int x = max(0, s[i] - 'a' - k);
+            int y = min(25, s[i] - 'a' + k);
+
+            int temp = 1;
 
             // Iterate through the range of characters
-            for(int j=x;j<=y;j++){
-                // Update temp with the maximum of its current value and 1 plus dp[j]
-                temp=max(temp,1+dp[j]);
+            for (int j = x; j <= y; j++) {
+                // Update temp with the maximum of its current value and 1 plus
+                // dp[j]
+                temp = max(temp, 1 + dp[j]);
             }
             // Update dp[s[i]-'a'] with temp
-            dp[s[i]-'a']=temp;
+            dp[s[i] - 'a'] = temp;
             // Update ans with the maximum of its current value and temp
-            ans=max(ans,temp);
+            ans = max(ans, temp);
         }
         return ans;
     }
