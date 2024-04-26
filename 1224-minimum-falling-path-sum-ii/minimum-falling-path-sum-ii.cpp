5e6b003ceeb10ab39cdc969590c2d36093f386e2
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-26 23:28:23 +0530

    Time: 27 ms (86.35%) | Memory: 16.9 MB (95.32%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,31 +1,36 @@
 class Solution {
- public:
-  int minFallingPathSum(vector<vector<int>>& grid) {
-    const int n = grid.size();
-
-    for (int i = 1; i < n; ++i) {
-      const vector<pair<int, int>> twoMinNumAndIndexs =
-          getTwoMinNumAndIndexs(grid[i - 1]);
-      const auto& [firstMinNum, firstMinIndex] = twoMinNumAndIndexs[0];
-      const auto& [secondMinNum, _] = twoMinNumAndIndexs[1];
-      for (int j = 0; j < n; ++j)
-        if (j == firstMinIndex)
-          grid[i][j] += secondMinNum;
-        else
-          grid[i][j] += firstMinNum;
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<int> sum(m, 0), minLeft(m, 0), minRight(m, 0);
+        for (int j = 0; j < m; j++) {
+            sum[j] = grid[0][j];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (j == 0) {
+                    minLeft[j] = sum[j];
+                } else {
+                    minLeft[j] = min(sum[j], minLeft[j-1]);
+                }
+            }
+            for (int j = m - 1; j >= 0; j--) {
+                if (j == m - 1) {
+                    minRight[j] = sum[j];
+                } else {
+                    minRight[j] = min(sum[j], minRight[j+1]);
+                }
+            }
+            for (int j = 0; j < m; j++) {
+                sum[j] = INT_MAX;
+                if (j-1 >= 0) sum[j] = min(sum[j], minLeft[j-1] + grid[i][j]);
+                if (j+1 < m) sum[j] = min(sum[j], minRight[j+1] + grid[i][j]);
+            }
+        }
+        int ans = INT_MAX;
+        for (auto val : sum) {
+            ans = min(ans, val);
+        }
+        return ans;
     }
-
-    return ranges::min(grid.back());
-  }
-
- private:
-  vector<pair<int, int>> getTwoMinNumAndIndexs(const vector<int>& A) {
-    vector<pair<int, int>> numAndIndexs;
-
-    for (int i = 0; i < A.size(); ++i)
-      numAndIndexs.emplace_back(A[i], i);
-
-    ranges::sort(numAndIndexs);
-    return {numAndIndexs[0], numAndIndexs[1]};
-  }
 };
