76059ebe0f9880d3fbd20ce9dcf62a574b65b9e1
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-04-30 12:17:31 +0530

    Time: 49 ms (79.64%) | Memory: 16.1 MB (80.84%) - LeetSync

--- 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     long long wonderfulSubstrings(string word) {
-        int cnt[1024] = {1};
-        long long ans = 0;
-        int st = 0;
-        for (char c : word) {
-            st ^= 1 << (c - 'a');
-            ans += cnt[st];
-            for (int i = 0; i < 10; ++i) {
-                ans += cnt[st ^ (1 << i)];
+        long long ans=0;
+        vector<int>cnt(1024,0);
+        cnt[0]=1;
+        int mask=0;
+        for(auto c: word)
+        {
+            mask= mask ^ (1<<(c-'a'));
+            ans+=cnt[mask];
+          
+            for(int i=0;i<10;i++)
+            {
+               int newmask= mask ^ (1<<i);
+                ans+=cnt[newmask];
             }
-            ++cnt[st];
+            cnt[mask]++;
         }
         return ans;
     }
