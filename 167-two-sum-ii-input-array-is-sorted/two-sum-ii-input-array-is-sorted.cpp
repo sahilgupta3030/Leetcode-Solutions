8e2b5ec6a720290c3182b81e3257f1f2a76b20b1
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-03 00:04:03 +0530

    Time: 9 ms (59.19%) | Memory: 18 MB (60.57%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int i = 0, j = numbers.size()-1;
-        
-        while(i < j) {
-            if(numbers[i]+numbers[j] == target) {
-                return {i+1, j+1};
-            }
-            
-            if(numbers[i]+numbers[j] > target)
-                j--;
-            else
-                i++;
+        return recursive(numbers, target, 0, numbers.size()-1);
+    }
+    
+    vector<int> recursive(vector<int>& numbers, int target, int i, int j) {
+        if (i >= j) {
+            return {};
         }
-        return {};
+        
+        if (numbers[i] + numbers[j] == target) {
+            return {i+1, j+1};
+        }
+        
+        if (numbers[i] + numbers[j] > target) {
+            return recursive(numbers, target, i, j-1);
+        } else {
+            return recursive(numbers, target, i+1, j);
+        }
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& numbers, int target) {
+//         int i = 0, j = numbers.size()-1;
+
+//         while(i < j) {
+//             if(numbers[i]+numbers[j] == target) {
+//                 return {i+1, j+1};
+//             }
+
+//             if(numbers[i]+numbers[j] > target)
+//                 j--;
+//             else
+//                 i++;
+//         }
+//         return {};
+//     }
+// };
