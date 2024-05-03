5cec17197912b0060761009628c9196a7eab1aeb
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-03 17:55:56 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (99.27%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
+    // Recursive function to find the winner's index in a circle of n people
+    int winner(int n, int k){
+        if(n == 1) return 0; // Base case: Only one person left
+        // Calculate winner's index for n-1 people, add k, and take modulo n
+        return (winner(n - 1, k) + k) % n;
+    }
+    
+    // Finds the winner's index (1-based) given the number of people (n) 
+    // and the count to skip (k)
     int findTheWinner(int n, int k) {
-        if (n == 1) return 1;
-        int ans = (findTheWinner(n - 1, k) + k) % n;
-        return ans == 0 ? n : ans;
+        return winner(n, k) + 1; // Add 1 to convert 0-based index to 1-based
     }
 };
