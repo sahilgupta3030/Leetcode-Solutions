49481a5a2acd359ff96e6efc218f75221428b6da
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-03 11:41:59 +0530

    Time: 3 ms (39.03%) | Memory: 8.1 MB (5.03%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    // Function to split version string into tokens
     vector<string> getTokens(string version) {
         stringstream ss(version);
         string token = "";
         vector<string> version_tokens;
 
+        // Splitting version string by '.'
         while (getline(ss, token, '.')) {
             version_tokens.push_back(token);
         }
@@ -12,8 +14,10 @@ public:
         return version_tokens;
     }
     
+    // Function to compare two version strings
     int compareVersion(string version1, string version2) {
 
+        // Getting tokens for both versions
         vector<string> v1 = getTokens(version1);
         vector<string> v2 = getTokens(version2);
 
@@ -21,18 +25,21 @@ public:
         int n = v2.size();
 
         int i = 0;
+        // Comparing corresponding tokens of both versions
         while (i < m || i < n) {
 
             int a = i < m ? stoi(v1[i]) : 0;
             int b = i < n ? stoi(v2[i]) : 0;
 
+            // Comparing token values
             if (a > b)
                 return 1;
             else if (b > a)
                 return -1;
             else
-                i++;
+                i++; // Move to next token if curr ones are equal
+
         }
-        return 0;
+        return 0; 
     }
 };
