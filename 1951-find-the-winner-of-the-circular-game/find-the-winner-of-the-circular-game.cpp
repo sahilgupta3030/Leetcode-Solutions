a8e8a7e6ec45237b923d8734282a779f67d12b4e
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-03 17:52:51 +0530

    Time: 2 ms (78.79%) | Memory: 7.1 MB (93.78%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
-    // Returns winner's index among n players with every k-th player removed
-    int findTheWinner(int n, int k) { return f(n, k) + 1; }
-
-private:
-    // Recursively finds winner after removing k-th player in each round
-    int f(int n, int k) {
-        if (n == 1)
-            return 0; // Index of the last player remaining
-        return (f(n - 1, k) + k) % n; // Next player's index after removal
+    int findTheWinner(int n, int k) {
+        if (n == 1) return 1;
+        int ans = (findTheWinner(n - 1, k) + k) % n;
+        return ans == 0 ? n : ans;
     }
 };
