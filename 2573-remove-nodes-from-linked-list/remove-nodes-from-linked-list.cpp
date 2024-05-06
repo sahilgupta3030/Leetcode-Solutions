e8e3465648a65af661f00a39056e8b300b378294
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-06 16:31:53 +0530

    Time: 333 ms (7.60%) | Memory: 170.8 MB (29.07%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -1,32 +1,56 @@
+// using Recursion..
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        stack<ListNode*> st;
-        ListNode* current = head;
-
-        while (current != nullptr) {
-            st.push(current);
-            current = current->next;
+        if (head == NULL || head->next == NULL) {
+            return head;
         }
 
-        current = st.top();
-        st.pop();
-        int maximum = current->val;
-        ListNode* resultHead = new ListNode(maximum);
+        ListNode* nextNode = removeNodes(head->next);
 
-        while (!st.empty()) {
-            current = st.top();
-            st.pop();
-            if (current->val < maximum) {
-                continue;
-            } else {
-                ListNode* newNode = new ListNode(current->val);
-                newNode->next = resultHead;
-                resultHead = newNode;
-                maximum = current->val;
-            }
+        if (head->val < nextNode->val) {
+            delete head;
+            return nextNode;
         }
 
-        return resultHead;
+        head->next = nextNode;
+        return head;
     }
 };
+
+
+// // using stack..
+// class Solution {
+// public:
+//     ListNode* removeNodes(ListNode* head) {
+//         stack<ListNode*> st;
+//         ListNode* current = head;
+
+//         while (current != nullptr) {
+//             st.push(current);
+//             current = current->next;
+//         }
+
+//         current = st.top();
+//         st.pop();
+//         int maximum = current->val;
+//         ListNode* resultHead = new ListNode(maximum);
+
+//         while (!st.empty()) {
+//             current = st.top();
+//             st.pop();
+//             if (current->val < maximum) {
+//                 continue;
+//             } else {
+//                 ListNode* newNode = new ListNode(current->val);
+//                 newNode->next = resultHead;
+//                 resultHead = newNode;
+//                 maximum = current->val;
+//             }
+//         }
+
+//         return resultHead;
+//     }
+// };
+
+
