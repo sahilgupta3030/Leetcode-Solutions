c596adec3ed023bbd1ee9ed1d5d5685c1aa8e5a9
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-07 15:58:52 +0530

    Time: 157 ms (92.91%) | Memory: 121.1 MB (50.70%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -1,38 +1,65 @@
-// Approach-1 (Using Reverse of LinkedList)
+// Approach-2 (Using Recursion)
 class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        if(head == NULL || head->next == NULL) {
-            return head;
-        }        
-        ListNode* last = reverseList(head->next);
-        head->next->next = head;
-        head->next = NULL;        
-        return last;
+public:    
+    int recursive_double(ListNode* head) {
+        if(head == NULL)
+            return 0;      
+
+        int carry  = recursive_double(head->next);
+        int newVal = (head->val)*2 + carry;
+        head->val = newVal % 10;    
+        return newVal / 10;        
     }
     
-    ListNode* doubleIt(ListNode* head) {
-        head = reverseList(head);
+    ListNode* doubleIt(ListNode* head) {        
+        int lastCarry = recursive_double(head);
         
-        ListNode* curr = head;
-        ListNode* prev = NULL;
-        int carry = 0;        
-        while(curr != NULL) {            
-            int newValue = curr->val * 2 + carry;
-            curr->val = newValue%10;
-            
-            if(newValue >= 10) {
-                carry = 1;
-            } else {
-                carry = 0;
-            }            
-            prev = curr;
-            curr = curr->next;            
+        if(lastCarry > 0) {
+            ListNode* newHead = new ListNode(lastCarry);
+            newHead->next = head;
+            return newHead;
         }        
-        if(carry != 0) {
-            ListNode* newHead = new ListNode(carry);
-            prev->next = newHead;
-        }
-        return reverseList(head);
+        return head;        
     }
 };
+
+
+
+// // Approach-1 (Using Reverse of LinkedList)
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         if(head == NULL || head->next == NULL) {
+//             return head;
+//         }        
+//         ListNode* last = reverseList(head->next);
+//         head->next->next = head;
+//         head->next = NULL;        
+//         return last;
+//     }
+    
+//     ListNode* doubleIt(ListNode* head) {
+//         head = reverseList(head);
+        
+//         ListNode* curr = head;
+//         ListNode* prev = NULL;
+//         int carry = 0;        
+//         while(curr != NULL) {            
+//             int newValue = curr->val * 2 + carry;
+//             curr->val = newValue%10;
+            
+//             if(newValue >= 10) {
+//                 carry = 1;
+//             } else {
+//                 carry = 0;
+//             }            
+//             prev = curr;
+//             curr = curr->next;            
+//         }        
+//         if(carry != 0) {
+//             ListNode* newHead = new ListNode(carry);
+//             prev->next = newHead;
+//         }
+//         return reverseList(head);
+//     }
+// };
