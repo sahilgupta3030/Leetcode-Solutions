372c4d32896d335111729e28e239e6092dab5778
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-08 12:10:36 +0530

    Time: 10 ms (51.87%) | Memory: 13.7 MB (82.42%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& scores) {
+        int numOfScores = scores.size();
+        vector<pair<int, int>> indexedScores;
+        for (int i = 0; i < numOfScores; ++i) {
+            indexedScores.push_back(make_pair(scores[i], i));
+        }
+        sort(indexedScores.begin(), indexedScores.end(), comparePairs);
+
+        vector<string> ranks(numOfScores);
+        vector<string> medals = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        for (int i = 0; i < numOfScores; ++i) {
+            if (i < 3) {
+                ranks[indexedScores[i].second] = medals[i];
+            } else {
+                ranks[indexedScores[i].second] = to_string(i + 1);
+            }
+        }
+        return ranks;
+    }
+
+    static bool comparePairs(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.first > b.first;
+    }
+};
