683dfcd694178b2ea8f2e9a7602d813571276f48
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-11 23:40:43 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (50.09%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,18 +1,19 @@
+// dynamic program..
+
+vector<int> dp(46, -1);
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 1) {
-            return 1;
+
+        // if result for n is already computed, return it.
+        if (dp[n] != -1)
+            return dp[n];
+
+        if (n <= 2) {
+            return dp[n] = n;
         }
-        
-        vector<int> dp(n + 1);
-        dp[0] = 1;
-        dp[1] = 1;
-        
-        for (int i = 2; i <= n; ++i) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        
-        return dp[n];
+
+        // recursively compute & store result for n using results for n-1 & n-2
+        return dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
