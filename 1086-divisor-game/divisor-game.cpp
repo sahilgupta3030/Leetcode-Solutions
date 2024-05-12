21630c8fde4da4fd1f50a44662242af884adad9a
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-12 22:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (23.43%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        // if(n<=1) return false;
-        // for(int x=1; x<n; x++){
-        //     if(n%x==0){
-        //         return !divisorGame(n-x);
-        //     }
-        // }
-        // return false;
-
-        if(n%2==0)return true;
+        if (n <= 1) {
+            return false;
+        }
+        for (int x = 1; x < n; x++) {
+            if (n % x == 0) {
+                return !divisorGame(n - x);
+            }
+        }
         return false;
+
+        // if(n%2==0)return true;
+        // return false;
     }
 };
