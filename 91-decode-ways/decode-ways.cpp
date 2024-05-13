57d910e8bcb2b6364e8b10d8fb20df40e2088834
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-13 13:11:07 +0530

    Time: 7 ms (7.07%) | Memory: 10.6 MB (13.93%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,8 +1,11 @@
+// for dp
 vector<int> dp(101, -1);
+
 class Solution {
 public:
     int n = 0;
     int numDecodingsHelper(string s, int pos) {
+        // base case
         if (pos == n)
             return 1;
         if (s[pos] == '0')
@@ -11,14 +14,19 @@ public:
         if (dp[pos] != -1)
             return dp[pos];
 
+        // taking 1num at a time
         int count = numDecodingsHelper(s, pos + 1);
+        // taking 2num at a time
         if (pos < n - 1 && s.substr(pos, 2) < "27") {
             count += numDecodingsHelper(s, pos + 2);
         }
         return dp[pos] = count;
     }
+
+    // main function..
     int numDecodings(string s) {
         n = s.size();
+
         fill(dp.begin(), dp.end(), -1);
         return numDecodingsHelper(s, 0);
     }
