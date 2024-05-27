537234df2414e2a324e697aca96715f5d1e85730
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-05-27 12:13:28 +0530

    Time: 5 ms (33.11%) | Memory: 10.1 MB (85.84%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -5,8 +5,12 @@ public:
         int n = nums.size();
 
         for (int x = 1; x <= n; ++x) {
-            auto it = lower_bound(nums.begin(), nums.end(), x);
-            int count = n - (it - nums.begin());
+            // Find the first element that is not less than x
+            auto firstNotLess = lower_bound(nums.begin(), nums.end(), x);
+
+            // Count the number of elements greater than or equal to x
+            int count = n - (firstNotLess - nums.begin());
+
             if (x == count) {
                 return x;
             }
