3db76ac1ca0d1f63346c45186b669c26dfe926c1
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-06-05 20:06:23 +0530

    Time: 6 ms (70.90%) | Memory: 11 MB (82.02%) - LeetSync

--- 1044-find-common-characters/find-common-characters.cpp
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -5,32 +5,25 @@ public:
             count[ch - 'a']++;
         }
     }
+
     vector<string> commonChars(vector<string>& words) {
         vector<string> result;
-
         int n = words.size();
 
         int count[26] = {0};
-
         fillCountArray(words[0], count);
 
         for (int i = 1; i < n; i++) {
             int temp[26] = {0};
-
             fillCountArray(words[i], temp);
-
             for (int i = 0; i < 26; i++) {
-                if (count[i] != temp[i])
-                    count[i] = min(count[i], temp[i]);
+                count[i] = min(count[i], temp[i]);
             }
         }
 
         for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                int c = count[i];
-                while (c--) {
-                    result.push_back(string(1, i + 'a'));
-                }
+            while (count[i]--) {
+                result.push_back(string(1, i + 'a'));
             }
         }
 
