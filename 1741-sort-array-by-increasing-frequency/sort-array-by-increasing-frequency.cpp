4842435bc28e0f0985694ce5d3d8de28e8c72709
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-06-10 16:57:55 +0530

    Time: 11 ms (17.52%) | Memory: 15 MB (25.04%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int, int> umap;
+
+        // Count frequencies of each number
         for (auto x : nums) {
             umap[x]++;
         }
 
+        // LAMBDA FUNCTION...
+
+        // Sort nums based on frequency, then by value
         sort(nums.begin(), nums.end(), [&](int a, int b) {
             return umap[a] != umap[b] ? umap[a] < umap[b] : a > b;
         });
