528d785978a89c2da6b60b677e4bf0f70c9b3d4f
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-06-11 16:11:43 +0530

    Time: 6 ms (17.85%) | Memory: 10.1 MB (41.93%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -6,17 +6,22 @@ public:
         map<int, int> mp;
         for (auto x : arr1)
             mp[x]++;
+        // Process elements in order of arr2
         for (auto temp : arr2) {
             if (mp.find(temp) != mp.end()) {
                 auto x = mp.find(temp);
                 int count = x->second;
 
+                // Create a vector with 'count' occurrences of 'temp'
                 vector<int> v(count, temp);
+                // Append to the result
                 ans.insert(ans.end(), v.begin(), v.end());
+                // Remove the element from the map
                 mp.erase(temp);
             }
         }
 
+        // Append remaining elements not in arr2, in ascending order
         for (auto x : mp) {
             int ele = x.first;
             int count = x.second;
