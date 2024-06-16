195f7f9fdd460327ecb23bcf2e9590c8dee0183e
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-06-16 14:27:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (78.53%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,16 +1,44 @@
-// Approach-1 (Perfect use of std::stringstream)
+// Approach-2 (Two Pointer)
 class Solution {
 public:
     string reverseWords(string s) {
-        stringstream ss(s);
-        string token = "";
-        string result = "";
-        
-        // By default stringstream tokenizes on space character
-        while (ss >> token) {
-            result = token + " " + result;
-        }
+        reverse(s.begin(), s.end());
 
-        return result.substr(0, result.length() - 1);
+        int i = 0;
+        int l = 0, r = 0;
+        int n = s.length();
+
+        while (i < n) {
+            while (i < n && s[i] != ' ') {
+                s[r++] = s[i++];
+            }
+
+            if (l < r) {
+                reverse(s.begin() + l, s.begin() + r);
+                s[r] = ' ';
+                r++;
+
+                l = r;
+            }
+            i++;
+        }
+        s = s.substr(0, r - 1);
+        return s;
     }
 };
+
+// // Approach-1 (Perfect use of std::stringstream)
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         stringstream ss(s);
+//         string token = "";
+//         string result = "";
+
+//         // By default stringstream tokenizes on space character
+//         while (ss >> token) {
+//             result = token + " " + result;
+//         }
+//         return result.substr(0, result.length() - 1);
+//     }
+// };
