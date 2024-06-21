4db5603184f8db4878d2d23b1b76d9d828db4fab
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-06-21 23:44:32 +0530

    Time: 26 ms (48.29%) | Memory: 34.3 MB (82.09%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -4,22 +4,20 @@ public:
         int n = customers.size();
         int unsat = 0;
 
-        // Calculate initial unsatisfied customers in the first 'minutes' window
+        // Initial unsatisfied customers in the first 'minutes' window
         for (int i = 0; i < minutes; i++) {
             unsat += customers[i] * grumpy[i];
         }
 
         int maxUnsat = unsat;
-
-        // Use two pointers i and j to define the sliding window of size 'minutes'
         int i = 0;
         int j = minutes;
 
         while(j < n) {
-            unsat += customers[j] * grumpy[j];             // Include current element
-            unsat -= customers[i] * grumpy[i];             // Remove element going out of window
+            unsat += customers[j] * grumpy[j]; // Include current element
+            unsat -= customers[i] * grumpy[i]; // Remove element going out of window
             
-            maxUnsat = max(maxUnsat, unsat);               // Update maxUnsat
+            maxUnsat = max(maxUnsat, unsat);   // Update maxUnsat
             i++;
             j++;
         }
