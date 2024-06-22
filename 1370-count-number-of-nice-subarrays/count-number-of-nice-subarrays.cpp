aaa9949648bfd53088114d743b9aced5339988f6
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-06-22 13:31:15 +0530

    Time: 99 ms (43.80%) | Memory: 73.1 MB (27.58%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int numberOfSubarrays(std::vector<int>& nums, int k) {
-        int size = nums.size(); // Store the size of the input vector nums
-        std::vector<int> count(size + 1, 0); // Vector to store the count of odd numbers
-        count[0] = 1; // There's one way to have zero odd numbers - empty subarray
+        int size = nums.size();
+        vector<int> count(size + 1, 0); 
+        count[0] = 1; 
 
-        int answer = 0; // Initialize the count of valid subarrays
-        int oddCount = 0; // Counter for the number of odd numbers in the current sequence
+        int answer = 0; 
+        int oddCount = 0; 
 
-        // Iterate through the input numbers
         for (int num : nums) {
-            oddCount += num & 1; // Increment the odd count if num is odd
-            // If there have been at least k odd numbers so far, update the answer
+            oddCount += num & 1;
             if (oddCount >= k) {
                 answer += count[oddCount - k];
             }
-            count[oddCount]++; // Increment the count for this number of odd numbers
+            count[oddCount]++; 
         }
       
-        return answer; // Return the total number of valid subarrays
+        return answer; 
     }
 };
