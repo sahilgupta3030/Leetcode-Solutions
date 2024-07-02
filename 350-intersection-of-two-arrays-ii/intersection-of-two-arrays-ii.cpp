56a47d19e4662c5bc7bb60a7d16257b0f2bdc486
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-07-02 11:13:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (44.72%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -3,17 +3,16 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
 
         vector<int> ans;
-        unordered_map<int, int> map1; // Renamed from count
+        unordered_map<int, int> map1;
 
         for (const int num : nums1)
             ++map1[num];
 
         for (const int num : nums2) {
             auto iterator = map1.find(num);
-            // iterator != map1.end() // means num not found
             if (iterator != map1.end() && iterator->second > 0) {
                 ans.push_back(num);
-                iterator->second--; // decrease the count of num in the map
+                iterator->second--; 
             }
         }
 
