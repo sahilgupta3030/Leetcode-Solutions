d9e3f68749eda1b4bf7434c8843c50b556e041f4
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-07-03 23:02:47 +0530

    Time: 3 ms (82.22%) | Memory: 12.1 MB (27.31%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-
-        unordered_map<int, int> mp;
-        int n = nums1.size();
-        vector<int> res(n);
+        unordered_map<int, int> umap;
         stack<int> st;
+        int n = nums2.size();
 
-        // Traverse nums2
-        for (int i = 0; i < nums2.size(); i++) {
-            // Find next greater element for current element in nums2
-            while (st.size() && st.top() < nums2[i]) {
-                mp[st.top()] = nums2[i];
+        for (int i = n - 1; i >= 0; i--) {
+            int ele = nums2[i];
+            while (!st.empty() && st.top() <= ele) {
                 st.pop();
             }
-            st.push(nums2[i]);
+            int res = (st.empty()) ? -1 : st.top();
+            umap.insert({ele, res});
+            st.push(ele);
         }
 
-        // Populate result vector with next greater elements
-        for (int i = 0; i < n; i++) {
-            if (mp[nums1[i]] == 0) {
-                res[i] = -1; // No next greater element found
-            } else {
-                res[i] = mp[nums1[i]]; // Set the next greater element found
-            }
+        vector<int> ans;
+        for (auto x : nums1) {
+            ans.push_back(umap[x]);
         }
-        return res;
+        return ans;
     }
 };
