a4d79277865048d4c72940bfab9f9a389fab8516
SAHIL GUPTA <sahilgupta3030@gmail.com>
2025-02-06 10:46:07 +0530

    Time: 379 ms (20.06%) | Memory: 85 MB (67.71%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        int ans = 0;
         unordered_map<int, int> count;
+        int result = 0;
 
-        for (int i = 0; i < nums.size(); ++i)
-            for (int j = 0; j < i; ++j)
-                ans += count[nums[i] * nums[j]]++ * 8;
+        // Loop through all pairs (i, j) where i > j
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                // Calculate product of pair
+                int product = nums[i] * nums[j];
 
-        return ans;
+                // If this product was seen before...
+                // ..add 8 times its count to the result
+                result += count[product] * 8;
+
+                // Increase the count of this product in the map
+                count[product]++;
+            }
+        }
+        return result;
     }
 };
