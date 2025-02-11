bc9ef988780b052f60647845a9ed518d56431908
SAHIL GUPTA <sahilgupta3030@gmail.com>
2025-02-11 12:00:38 +0530

    Time: 487 ms (5.85%) | Memory: 423.1 MB (5.32%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,51 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
+        stack<char> st;
         int m = part.size();
-        while (s.find(part) != -1) {
-            s = s.erase(s.find(part), m);
+
+        for (char c : s) {
+            st.push(c);
+
+            // Check if top characters match 'part'
+            if (st.size() >= m) {
+                string temp = "";
+                for (int i = 0; i < m; ++i) {
+                    temp = st.top() + temp;
+                    st.pop();
+                }
+
+                // If not equal to 'part', push characters back
+                if (temp != part) {
+                    for (char ch : temp) {
+                        st.push(ch);
+                    }
+                }
+            }
         }
-        return s;
+
+        // Construct result string from stack
+        string result = "";
+        while (!st.empty()) {
+            result = st.top() + result;
+            st.pop();
+        }
+
+        return result;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     string removeOccurrences(string s, string part) {
+//         int m = part.size();
+//         while (s.find(part) != -1) {
+//             s = s.erase(s.find(part), m);
+//         }
+//         return s;
+//     }
+// };
