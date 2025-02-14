1a453195b87cef4ac45b96373eaf76bbed6bbced
SAHIL GUPTA <sahilgupta3030@gmail.com>
2025-02-14 18:15:14 +0530

    Time: 10 ms (95.51%) | Memory: 77.9 MB (77.84%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,19 +1,20 @@
 class ProductOfNumbers {
- public:
-  ProductOfNumbers() : prefix{1} {}
+public:
+    ProductOfNumbers() : prefix{1} {}
 
-  void add(int num) {
-    if (num == 0)
-      prefix = {1};
-    else
-      prefix.push_back(prefix.back() * num);
-  }
+    void add(int num) {
+        if (num == 0)
+            prefix = {1};
+        else
+            prefix.push_back(prefix.back() * num);
+    }
 
-  int getProduct(int k) {
-    return k >= prefix.size() ? 0
-                              : prefix.back() / prefix[prefix.size() - k - 1];
-  }
+    int getProduct(int k) {
+        return k >= prefix.size()
+                   ? 0
+                   : prefix.back() / prefix[prefix.size() - k - 1];
+    }
 
- private:
-  vector<int> prefix;
+private:
+    vector<int> prefix;
 };
