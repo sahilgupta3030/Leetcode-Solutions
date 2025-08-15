bd42ae77ac2d70cbdb08db398998f6cf2fd8bdb2
SAHIL GUPTA <sahilgupta3030@gmail.com>
2025-08-15 11:33:35 +0530

    Time: 3 ms (2.82%) | Memory: 8.6 MB (12.05%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,11 +1,32 @@
+// class Solution {
+// public:
+//     int hammingWeight(int n) {
+//         int count = 0;
+
+//         while (n > 0) {
+//             count += (n & 1); // add 1 if last bit is set
+//             n >>= 1;          // shift right by 1
+//         }
+
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int hammingWeight(int n) {
         int count = 0;
 
+        string binForm = "";
         while (n > 0) {
-            count += (n & 1); // add 1 if last bit is set
-            n >>= 1;          // shift right by 1
+            binForm = char((n % 2) + '0') + binForm;
+            n /= 2;
+        }
+
+        for (char bit : binForm) {
+            if (bit == '1') {
+                count++;
+            }
         }
 
         return count;
